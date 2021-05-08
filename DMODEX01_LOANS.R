aae72ea11e75a2693e51f16c3674566830b00241
Marc25 <cestudios.alonso@gmail.com>
2021-05-08 16:01:57 +0000

    Add train an testing object examples

--- DMODEX01_LOANS.R
+++ DMODEX01_LOANS.R
@@ -3,6 +3,8 @@
 # Librarys ----
 library(tidymodels)
 library(ggplot2)
+library(tidymodels)
+
 # Parameters ----
 
 # Data
@@ -16,12 +18,29 @@ g1 <- ggplot(loan_df, aes(loan_default, ..count.., fill=loan_default))+
 g2 <- ggplot(loan_df, aes(interest_rate, fill=loan_default))+
   geom_density(alpha= 0.3)
 
+# Train y Test data ----
 loan_split <- loan_df %>% initial_split(loan_df, prop = 0.8 ,strata = loan_default)
 loan_split
 
 loan_training <- loan_split %>% training()
+loan_test <- loan_split %>% testing()
+
+# Specify a logistic regression model ----
+logistic_model <- logistic_reg() %>% 
+  # Set the engine
+  set_engine('glm') %>% 
+  # Set the mode
+  set_mode('classification')
+# Fit to training data
+logistic_fit <- logistic_model %>% 
+  fit(loan_default ~ . ,
+      data = loan_training)
+# Print model fit object
+# print(logistic_fit)
+
 
 reportS <- list(data = loan_df , 
                 data_train = loan_training, 
+                model1 = logistic_fit, 
                 g1 = g1, 
                 g2 = g2)
--- MainEx.R
+++ MainEx.R
@@ -1,5 +1,8 @@
 library('rmarkdown')
+library(ggplot2)
+rm(list=ls())
 source("DMODEX01_LOANS.R")
+
 # Automate
 # How can you automate the process of opening the document and changing the parameter value by hand?
 # You can supply a different value via the params argument of render() from the {rmarkdown} package:
