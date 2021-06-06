4ceee02b49d25b8baefa0d716d387a63bce06d49
Marc25 <cestudios.alonso@gmail.com>
2021-06-06 09:24:35 +0000

    Read Data

--- DGINIEX01_MYGL.R
+++ DGINIEX01_MYGL.R
@@ -1,3 +1,13 @@
 # My Project Example Gini and Lorentz tutorial
 # Data : Url Source ----
 # https://jackblun.github.io/Globalinc/ 
+# Librarys ----
+library(data.table)
+path1 <-  c("/cloud/project/data/")
+#Global Income Distribution since 1980
+data <- read.csv2(paste0(path1,"GCIPrawdata.txt"), sep=",")
+View(data)
+data <- data.table(data)
+data[which.max(Decile.9.Income),]
+data[which.max(Mean.Income),]
+
